65d23c775033da7fe6b2e70b2027a0d505635fc9
zzpjw <zzpjw9710@naver.com>
2022-04-02 00:04:22 +0900

    16/16 end commit

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ class Site {
   }
 
   addBoard(board) {
-    // board.boardName === this.findBoardByName(board.boardName).boardName
     if (this.findBoardByName(board.boardName)) {
       throw new Error("중복");
     }
+    board.myBoard = true;
     return this.boards.push(board);
   }
 }
@@ -23,48 +23,70 @@ class Board {
     } else {
       this.boardName = boardName;
       this.articles = [];
+      this.myBoard = false;
       // this.boardSite =
     }
   }
 
   publish(article) {
-    // console.log(this.boardName)
-    // if ()********************************
-    return this.articles.push(article)
+    if (!this.myBoard)
+      throw new Error("Site에 추가되지 않음");
+    else {
+      article.id = `${this.boardName}-`+Math.random()
+      article.createdDate = new Date().toISOString()
+      this.articles.push(article)
+    }
+
   }
 
   getAllArticles() {
-    // return this.boardName
+    return this.articles
   }
 
 }
 
 class Article {
-  constructor(subject, content, author) {
-    if (subject === '' || content === '' || author === '' || subject === null || content === null || author === null) {
-      throw new Error('subject, content, author 정보없음');
-    } else {
-      this.ArticleSubject = subject;
-      this.ArticleContent = content;
-      this.ArticleAuthor = author;
+  //article은 object로 들어옴. 따라서 분리해주어야 함. {}도 씌워주어야 함.
+  constructor({subject, content, author}) {
+    if (subject === '' || subject === null) {
+      throw new Error('subject 정보없음');
+    }
+    else if (content === '' || content === null) {
+      throw new Error('content 정보없음');
+    }
+    else if (author === '' || author === null) {
+      throw new Error('author 정보없음');
+    }
+    else {
+      this.subject = subject;
+      this.content = content;
+      this.author = author
+      this.comments = []
     }
   }
 
   getAllComments() {
+    return this.comments
   }
 
-  reply() {
+  reply(comment) {
+    if (!this.id)
+      throw new Error("article에 추가되지 않음");
+    else {
+      comment.createdDate = new Date().toISOString()
+      this.comments.push(comment)
+    }
   }
 }
 
 
 class Comment {
-  constructor(content, author) {
+  constructor({content, author}) {
     if (content === "" || content === null || author === "" || author === null) {
       throw new Error("Comment 정보없음");
     } else {
-      this.CommentContent = content;
-      this.CommentAuthor = author;
+      this.content = content;
+      this.author = author;
     }
   }
 }
